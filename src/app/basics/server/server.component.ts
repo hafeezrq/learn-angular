58e126913f808e3f6b188ba716316123172762f9
hafeezrq <hafeezrq@yahoo.com>
2023-03-11 13:21:11 +1100

    Demo string interpolation.

--- src/app/basics/server/server.component.ts
+++ src/app/basics/server/server.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
+  serverName: string = 'Server ONE';
+  serverId: number = 10;
+  serverStatus: string = 'Offline';
 
+  getServerName() {
+    return this.serverName;
+  }
 }
