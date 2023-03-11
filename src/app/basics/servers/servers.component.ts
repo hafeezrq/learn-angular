bd90cb27dd256612d1de9bd0a80717a986ac1575
hafeezrq <hafeezrq@yahoo.com>
2023-03-11 14:46:44 +1100

    Demo: html element's propery binding.

--- src/app/basics/servers/servers.component.ts
+++ src/app/basics/servers/servers.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowServerAdd = true;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowServerAdd = false;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
 }
