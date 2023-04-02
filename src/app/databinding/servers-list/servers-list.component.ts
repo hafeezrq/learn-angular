d1a7ddff9b2c09518c4fccb2ccff377a8723ea0d
hafeezrq <hafeezrq@yahoo.com>
2023-04-02 15:15:34 +1000

    Demo comunication bet. parent and child.

--- src/app/databinding/servers-list/servers-list.component.ts
+++ src/app/databinding/servers-list/servers-list.component.ts
@@ -9,4 +9,23 @@ export class ServersListComponent {
   serverElements = [
     { type: 'server', name: 'testServer', content: 'Just a test server' },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
