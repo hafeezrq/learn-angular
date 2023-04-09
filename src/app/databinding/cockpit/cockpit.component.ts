1e3e0365afa37ac0a905a1404d0ad9edba7877f4
hafeezrq <hafeezrq@yahoo.com>
2023-04-09 11:16:06 +1000

    Demo local ref in template.

--- src/app/databinding/cockpit/cockpit.component.ts
+++ src/app/databinding/cockpit/cockpit.component.ts
@@ -22,16 +22,18 @@ export class CockpitComponent implements OnInit {
     throw new Error('Method not implemented.');
   }
 
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      // serverName: this.newServerName,
+      serverName: serverNameInput.value,
       serverContent: this.newServerContent,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      // serverName: this.newServerName,
+      serverName: serverNameInput.value,
       serverContent: this.newServerContent,
     });
   }
